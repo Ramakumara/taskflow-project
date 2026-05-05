522e8f7609338b9aa79789f601a60a1ce6b6b533
Ram <ramkumarram6073@gmail.com>
2026-05-05 18:05:42 +0530

    small changes in backend

--- backend/auth_utils.py
+++ backend/auth_utils.py
@@ -11,7 +11,7 @@ router = APIRouter()
 
 SECRET_KEY = "your_secret_key_here"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+ACCESS_TOKEN_EXPIRE_MINUTES = 240
 conf = ConnectionConfig(
     MAIL_USERNAME="ramkumarram6073@gmail.com",
     MAIL_PASSWORD="ukfz xxsb sndp spfz",
